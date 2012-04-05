1729f35945908499d69506d43970e71de71a95ab
Jeremy Granadillo <akakitsu@gmail.com>
2012-04-05 14:18:55 +0200

    Merged buttons.js

--- extern/final.js
+++ extern/final.js
@@ -14,6 +14,11 @@ function Constructor(){
 	ShowTrackData();
         getPlaylistXML(Get_User_ID());
         createTimeline();
+        AddToTimeline();
+        PlayTimeline();
+        SubButton();
+        Cancel();
+        DivEvent();
 
         console.log('Finished: loading app.');
 }
@@ -217,7 +222,6 @@ function createEntries()
             i++;
     }
 
-    DivEvent();
 }
 
 function TrackData(){
@@ -256,7 +260,7 @@ function TracksAmount(){
 }
 
 function DivEvent(){
-    $(".timelineEntry").on('click',function(){
+    $("#timelineEntrys").on('click','.timelineEntry',function(){
             PlayTrack(trackInfo[this.id][2]);
             });
 }
@@ -282,4 +286,69 @@ var uri = 'http://open.spotify.com/user/1117535795/playlist/59nkn5pKC1SMtRr5mG2J
 
 models.player.play(uri);
 
+}
+
+function AddToTimeline()
+{
+        $("#AddToTimeline").click(function()
+        {
+            console.log('Started: AddToTimeline');
+            $("#canvas").hide();
+            $("#timeline").hide();
+            $("#timelineEntrys").hide();
+            $("form").show();
+        });
+}
+
+function SubButton()
+{
+    $("#SubButton").click(function()
+    {
+        console.log('Started: SubButton');
+        var track = player.track;
+        var full_t_uri = track.uri;
+        // The track uri is given in the form:"spotify:track:2r97a73n56d0m.."
+        // We need to split it up and select the important part
+        var t_uri_array = full_t_uri.split(":");
+        var t_uri = t_uri_array[2];
+        var textcomment = $("textarea").val();
+        var userid = Get_User_ID();
+        var base_url = "http://student.cmi.hro.nl/0851729/prj3/ultify/ultify.php?u=";
+        var get_url = base_url+userid;
+        $.post(
+        get_url,
+        {track:t_uri,comment:textcomment,submit:"submit"},
+        // TODO: When we have a function to refresh timeline,
+        // change this function
+        function(data)
+        {
+            console.log(data);
+        },
+        "xml");
+        $("#canvas").show();
+        $("#timeline").show();
+        $("#timelineEntrys").show();
+        $("form").hide();
+    });
+}
+
+function PlayTimeline()
+{
+    $("#PlayTimeline").click(function()
+        {
+             console.log('Started: PlayTimeline');
+             Constructor_Playlist();
+        });
+}
+
+function Cancel()
+{
+    $("#Cancel").click(function()
+        {
+            console.log('Started: Cancel');
+            $("#canvas").show();
+            $("#timeline").show();
+            $("#timelineEntrys").show();
+            $("form").hide();
+        });
 }
