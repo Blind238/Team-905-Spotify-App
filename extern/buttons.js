0ff8644c5c265db82ff8eaa41c6c1803597e553d
Ginn Ok1 <filip_ok1@live.nl>
2012-04-05 15:23:19 +0200

    Merge branch 'master' of github.com:Blind-238/Team-905-Spotify-App into HEAD
    Conflicts:
    	index.html

--- extern/buttons.js
+++ extern/buttons.js
@@ -5,6 +5,9 @@ function Constructor()
     AddToTimeline();
     PlayTimeline();
     SubButton();
+    Cancel();
+    
+    
 }
 
 function AddToTimeline()
@@ -13,6 +16,8 @@ function AddToTimeline()
         {
             console.log('Started: AddToTimeline');
             $("#canvas").hide();
+            $("#timeline").hide();
+            $("#timelineEntrys").hide();
             $("form").show();
         });
 }
@@ -22,12 +27,50 @@ function SubButton()
     $("#SubButton").click(function()
     {
         console.log('Started: SubButton');
+        var track = player.track;
+        var full_t_uri = track.uri;
+        // The track uri is given in the form:"spotify:track:2r97a73n56d0m.."
+        // We need to split it up and select the important part
+        var t_uri_array = full_t_uri.split(":");
+        var t_uri = t_uri_array[2];
+        var textcomment = $("textarea").val();
+        var userid = Get_User_ID();
+        var base_url = "http://student.cmi.hro.nl/0851729/prj3/ultify/ultify.php?u=";
+        var get_url = base_url+userid;
+        $.post(
+        get_url,
+        {track:t_uri,comment:textcomment,submit:"submit"},
+        // TODO: When we have a function to refresh timeline,
+        // change this function
+        function(data)
+        {
+            console.log(data);
+        },
+        "xml");
         $("#canvas").show();
+        $("#timeline").show();
+        $("#timelineEntrys").show();
         $("form").hide();
     });
 }
 
 function PlayTimeline()
 {
-    console.log('Started: PlayTimeline');
+    $("#PlayTimeline").click(function()
+        {
+             console.log('Started: PlayTimeline');
+             Constructor_Playlist();
+        });
+}
+
+function Cancel()
+{
+    $("#Cancel").click(function()
+        {
+            console.log('Started: Cancel');
+            $("#canvas").show();
+            $("#timeline").show();
+            $("#timelineEntrys").show();
+            $("form").hide();
+        });
 }
--- extern/final.js
+++ extern/final.js
@@ -11,46 +11,16 @@ $(document).ready(Constructor);
 function Constructor(){
 
         console.log('Started: loading app.');
-	//PlayerRefresh();
-	CurrentSongData();
 	ShowTrackData();
-	//Get_User_ID();
         getPlaylistXML(Get_User_ID());
         createTimeline();
+        AddToTimeline();
+        PlayTimeline();
+        SubButton();
+        Cancel();
         DivEvent();
-        
-        $(".timelineEntry").on('click',function(){
-            //var divID = $(this).id.text()
-            console.log(this.id);
-            console.log(trackInfo[this.id][0]);
-            console.log(trackInfo[this.id][1]);
-            console.log(trackInfo[this.id][2]);
-            //console.log();
-            console.log(trackInfo[this.id][2]);
-            PlayTrack(trackInfo[this.id][2]);
-            });
-        
-	console.log('Finished: loading app.');
-        
-        $("h2").ajaxError(function(event,request,settings,error){
-            console.log("AJAX error:");
-            console.log("event:" + event);
-            console.log("request:" + request);
-            console.log("settings:"+ settings);
-            if (error != null)
-            {
-                console.log("error:" + error);
-            }
-            console.log("AJAX error end.");
-        });
-}
 
-function PlayerRefresh(){
-	player.observe(models.EVENT.CHANGE, function (e){
-			// Only update the page if the track changed
-			ReloadPage();
-			console.log('Started: PlayerRefresh');
-    });
+        console.log('Finished: loading app.');
 }
 
 function CreatePlaylist(){
@@ -65,22 +35,6 @@ function CreatePlaylist(){
 		console.log(PlayList[i].data.name);
 	}
 }
-
-/*function Test(){
-	console.log('Started: Test');
-}*/
-
-/*function CurrentSongData(){
-	console.log('Started: CurrentSongData');
-	var PlayerTrackInfo = player.track;
-	var Track = PlayerTrackInfo.data;
-}*/
-
-/*function CurrentPlaylist(){
-	console.log('Started: CurrentPlaylist');
-	nummers[i] = array[i].data.uri;
-	console.log("nummers: " +nummers);
-}*/
 
 function ReloadPage(){
 	console.log('Started: ReloadPage');
@@ -125,56 +79,37 @@ function Squares(){
 }
 
 function PlayTrack(uri){
-	console.log('Started: PlaySail');
+	console.log('Started: PlayTrack');
 	//var uri = 'spotify:track:4VUGq8KUTVv5YnMqU6nkDa';
 	sp.trackPlayer.playTrackFromUri(uri, {
 		onSuccess: function() { console.log("success SONG");} ,
 		onFailure: function () { console.log("failure SONG");},
 		onComplete: function () { console.log("complete SONG"); }
     });
+	
+}
 
-}*/
 function Get_User_ID(){
 	
 	console.log('Started: Get_User_ID');
 	var ID = models.session.anonymousUserID;
-	console.log(ID);
         return ID;
-        console.log('Finished: Get_User_ID');
 }
 
 function Constructor_Playlist(){
 	
-	console.log('Started playlist constructor.');
-	//GetPlaylist();
-	//GetTracksPlaylist();
-	//AddPlaylist();
-	//RemoveTracks();
+	console.log('Started: playlist constructor.');
+	RemoveTracks();
 	AddTracks();
-	console.log('Finished playlist constructor.');
+	console.log('Finished: playlist constructor.');
 }
 
 function GetPlaylist(){
 	
 	console.log('Started: GetPlaylist');
 	var Playlist = new m.Playlist.fromURI('http://open.spotify.com/user/1117535795/playlist/59nkn5pKC1SMtRr5mG2JPm');
-	console.log('Got playlist: ' + Playlist.name);
 	return Playlist
 }
-
-function AddPlaylist(){
-	console.log('Started: AddPlaylist');
-	var Playlist = new m.Playlist.fromURI('http://open.spotify.com/user/1117535795/playlist/59nkn5pKC1SMtRr5mG2JPm');
-	Playlist.subscribed = true;
-	console.log('Playlist added to account');
-}
-
-/*function GetTracksPlaylist(){
-
-	console.log('Started: GetTracksPlaylist');
-	var Playlist = GetPlaylist();
-	var Tracks = Playlist.tracks;
-}*/
 
 function RemoveTracks(){
 	console.log('Started: RemoveTracks');
@@ -194,12 +129,8 @@ function GetNewTracks(){
 	var NewTracks = new Array();
         $(xmlData).find("track_uri").each(function(){
             NewTracks.push($(this).text());
-            console.log("Pushing to NewTracks: " +$(this).text());
         });
-        
-	//var NewTracks = ["4VUGq8KUTVv5YnMqU6nkDa","4d5QDE01i4iYVpPOfG6ho5","59OoFabb4932QQUqcY7awO"];
 	return NewTracks;
-	
 }
 
 function AddTracks(){
@@ -215,30 +146,14 @@ function AddTracks(){
 		Playlist.add('spotify:track:' + TrackURI);
 		i++;
 	}
-	
-	console.log('Tracks added');
-	
 }
 
 function getPlaylistXML(user_id,refresh){
-    // user_id is unique user and app identifier.
-    // refresh is passed if a the data needs to retrieved again
-    //  (after an INSERT or UPDATE)
     console.log("Started: processPlaylistXML");
     var base_url="http://student.cmi.hro.nl/0851729/prj3/ultify/ultify.php";
-    console.log("url: "+base_url);
-    console.log("refresh: "+refresh);
+
     if (refresh == 1 || xmlData == undefined)
     {
-        console.log("retreiving xml because:");
-        if (refresh==1)
-        {
-            console.log("refresh == 1.");
-        }
-        else if(xmlData== undefined)
-        {
-            console.log("xmlData == undefined.");
-        }
         $.get
         (
             base_url,
@@ -249,35 +164,24 @@ function getPlaylistXML(user_id,refresh){
     } 
     else 
     {
-        console.log("not refreshing");
         processPlaylistXML(xmlData);
     }
 }
 
 function processPlaylistXML(xml){
-    console.log("AJAX call finished")
     xmlData = xml;
-    var i=0
-    /*$(xml).find("comment").each(function(){
-        $("#timeline").append($(this).text()+i);
-        i++;
-    });*/
     createTimeline();
 }
 
 function createTimeline()
 {
     console.log('Started: createTimeline');
-    //console.log(xmlData);
     var timeline = document.getElementById("timeline");
     var ctx = timeline.getContext("2d");
     
-    //Creates the basic timeline
     ctx.beginPath();
-    ctx.moveTo(0,50);
-    ctx.lineTo(800,50);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "#FF3300";
+    ctx.moveTo(0,100);
+    ctx.lineTo(800,100);
     ctx.stroke();
     TrackData();
 }
@@ -289,18 +193,17 @@ function createEntries()
     var timeline = document.getElementById("timeline");
     var ctx = timeline.getContext("2d");
     var i = 0;
-    
+
+    console.log("Started: createEntries");
     while (i < 5)
     {
-            console.log("Started: createEntries");
-
+            
             var spacing = (800 / entryMax) * i;
             var Entryspacing = (800 / entryMax) - 150;
-            
+
             ctx.beginPath();
-            ctx.moveTo(spacing,0);
+            ctx.moveTo(spacing,10);
             ctx.lineTo(spacing,50);
-            ctx.lineWidth = 5;
             ctx.strokeStyle = "#FF3300";
             ctx.stroke();
 
@@ -317,43 +220,34 @@ function createEntries()
                                 .css('margin-right', Entryspacing);
             
             i++;
-            
-            console.log("createEntries:" + i)
     }
-    DivEvent();
+
 }
 
 function TrackData(){
-    // If length is bigger than 0, there's data in it.
-    // We need it to be empty
+
     if (trackInfo.length>0){
         trackInfo = [];
     }
     var TrackData = new Array();
+
     $(xmlData).find("track").each(function(){
         var track_uri = $(this).find("track_uri").text();
+
         m.Track.fromURI("spotify:track:"+track_uri,function(track){
             CheckData(track.name,track.album.artist.name,track.uri);
-            //TrackData.push(track.name);
-            //TrackData.push(track.artists);
-            //TrackData.push(track.album.artist);
         });
     })
-    //console.log(TrackData);
-    //return TrackData;
 }
 
 function CheckData(track,artist,uri){
+
     var temp = [track,artist,uri];
     trackInfo.push(temp);
-    console.log("trackInfo.length == " + trackInfo.length);
-    console.log("pushing " + temp);
+
     if (trackInfo.length == TracksAmount()){
-        console.log("trackInfo.length is equal to TracksAmount")
         createEntries();
-    } else if (trackInfo.length > TracksAmount()){
-        console.log("trackInfo.length is bigger than TracksAmount")
-    }
+    } 
 }
 
 function TracksAmount(){
@@ -362,19 +256,11 @@ function TracksAmount(){
     $(xmlData).find("track").each(function(){
         i++;
     })
-    console.log("TracksAmount returns: "+i);
     return i;
 }
 
 function DivEvent(){
-    $(".timelineEntry").on('click',function(){
-            //var divID = $(this).id.text()
-            console.log(this.id);
-            console.log(trackInfo[this.id][0]);
-            console.log(trackInfo[this.id][1]);
-            console.log(trackInfo[this.id][2]);
-            //console.log();
-            console.log(trackInfo[this.id][2]);
+    $("#timelineEntrys").on('click','.timelineEntry',function(){
             PlayTrack(trackInfo[this.id][2]);
             });
 }
@@ -390,4 +276,79 @@ function HelpMe(){
         item.style.visibility = 'visible';
     }
 
+}
+
+function PlayPlaylist(){
+
+console.log("Started: PlayPlaylist");
+
+var uri = 'http://open.spotify.com/user/1117535795/playlist/59nkn5pKC1SMtRr5mG2JPm';
+
+models.player.play(uri);
+
+}
+
+function AddToTimeline()
+{
+        $("#AddToTimeline").click(function()
+        {
+            console.log('Started: AddToTimeline');
+            $("#canvas").hide();
+            $("#timeline").hide();
+            $("#timelineEntrys").hide();
+            $("form").show();
+        });
+}
+
+function SubButton()
+{
+    $("#SubButton").click(function()
+    {
+        console.log('Started: SubButton');
+        var track = player.track;
+        var full_t_uri = track.uri;
+        // The track uri is given in the form:"spotify:track:2r97a73n56d0m.."
+        // We need to split it up and select the important part
+        var t_uri_array = full_t_uri.split(":");
+        var t_uri = t_uri_array[2];
+        var textcomment = $("textarea").val();
+        var userid = Get_User_ID();
+        var base_url = "http://student.cmi.hro.nl/0851729/prj3/ultify/ultify.php?u=";
+        var get_url = base_url+userid;
+        $.post(
+        get_url,
+        {track:t_uri,comment:textcomment,submit:"submit"},
+        // TODO: When we have a function to refresh timeline,
+        // change this function
+        function(data)
+        {
+            console.log(data);
+        },
+        "xml");
+        $("#canvas").show();
+        $("#timeline").show();
+        $("#timelineEntrys").show();
+        $("form").hide();
+    });
+}
+
+function PlayTimeline()
+{
+    $("#PlayTimeline").click(function()
+        {
+             console.log('Started: PlayTimeline');
+             Constructor_Playlist();
+        });
+}
+
+function Cancel()
+{
+    $("#Cancel").click(function()
+        {
+            console.log('Started: Cancel');
+            $("#canvas").show();
+            $("#timeline").show();
+            $("#timelineEntrys").show();
+            $("form").hide();
+        });
 }
